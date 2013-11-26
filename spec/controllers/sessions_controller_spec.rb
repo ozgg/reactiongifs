d8989f943b5ed11980b761f5eb06627ef6e5ee1b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-11-26 15:17:47 +0400

    Empty sessions controller and pending specs

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe SessionsController do
+  context 'Anonymous user' do
+    describe 'get new' do
+      it 'shows authorization form'
+    end
+
+    describe 'post create' do
+      it 'redirects to root page with valid credentials'
+      it 'redirects to login form with invalid credentials'
+    end
+
+    describe 'delete destroy' do
+      it 'redirects to root page'
+      it 'adds flash message "already unauthorized"'
+    end
+  end
+
+  context 'Successful login' do
+    it 'creates session with user id'
+    it 'adds flash message "logged in successfully"'
+  end
+
+  context 'Authorized user' do
+    describe 'get new' do
+      it 'redirects to root page'
+      it 'adds flash message "already authorized"'
+    end
+
+    describe 'post create' do
+      it 'redirects to root page'
+      it 'adds flash message "already authorized"'
+    end
+
+    describe 'delete destroy' do
+      it 'deletes user id from session'
+      it 'redirects to root page'
+      it 'adds flash message "logged out"'
+    end
+  end
+end
