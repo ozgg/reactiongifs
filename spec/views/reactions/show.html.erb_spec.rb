6bab90937c5651e9e4d76632245ac6f2f985462d
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-07 17:54:21 +0400

    Show reaction view

--- spec/views/reactions/show.html.erb_spec.rb
+++ spec/views/reactions/show.html.erb_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe 'reactions/show.html.erb' do
-  it 'displays reaction title and image'
+  let(:user) { User.create! login: 'some_guy', password: '123', password_confirmation: '123' }
+  let(:reaction) do
+    Reaction.create!(
+        user:  user,
+        title: 'Something happens',
+        image: Rack::Test::UploadedFile.new('spec/support/images/magic.gif', 'image/gif')
+    )
+  end
+
+  before :each do
+    assign :reaction, reaction
+    render
+  end
+
+  it 'displays reaction title' do
+    expect(rendered).to contain(reaction.title)
+  end
+
+  it 'displays reaction image' do
+    expect(rendered).to have_selector('img', src: reaction.image.url)
+  end
 end
