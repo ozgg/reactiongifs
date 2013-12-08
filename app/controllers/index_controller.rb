0252f092e4b8186e15b9c85f60b93b35aafd1f25
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-09 01:55:49 +0400

    Reactions on front with CSS

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,8 @@
 class IndexController < ApplicationController
   def index
-    @reactions = Reaction.order('id desc').page(params[:page] || 1).per(5)
+    page   = params[:page] || 1
+    @title = "Реакции в GIF-картинках, страница #{page}"
+
+    @reactions = Reaction.order('id desc').page(page).per(5)
   end
 end
