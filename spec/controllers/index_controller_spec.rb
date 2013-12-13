3734a9263d06d36b8f2e7cec337827e8891f3e14
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-14 00:42:32 +0400

    shared examples for index_controller_spec

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe IndexController do
-  context "in all cases" do
-    describe "get index" do
-      it "renders index/index view" do
-        get :index
-        expect(response).to render_template('index/index')
-      end
+  shared_examples "index/index view renderer" do
+    it "renders index/index view" do
+      get :index
+      expect(response).to render_template('index/index')
     end
   end
 
   context "when reactions found" do
     describe "get index" do
+      it_should_behave_like "index/index view renderer"
+
       it "assigns @reactions with approved reactions" do
         reaction = FactoryGirl.create(:approved_reaction)
 
@@ -30,6 +30,8 @@ describe IndexController do
 
   context "when no reactions found" do
     describe "get index" do
+      it_should_behave_like "index/index view renderer"
+
       it "assigns empty @reactions" do
         get :index
         expect(assigns[:reactions]).to be_empty
