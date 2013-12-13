7dc7aedb6d645a21d1a32560b53f9dbfb1516f95
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-13 22:41:14 +0400

    Approved reaction factory

--- spec/factories/Reaction.rb
+++ spec/factories/Reaction.rb
@@ -4,4 +4,11 @@ FactoryGirl.define do
     title 'Something happens'
     image Rack::Test::UploadedFile.new('spec/support/images/magic.gif', 'image/gif')
   end
+
+  factory :approved_reaction, class: Reaction do
+    user
+    title 'Something approved happens'
+    image Rack::Test::UploadedFile.new('spec/support/images/magic2.gif', 'image/gif')
+    approved true
+  end
 end
