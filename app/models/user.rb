7c34cdb4299aacee21b786f533a02d9b7a5e2a30
Maxim Khan-Magomedov <maxim.km@gmail.com>
2013-12-23 18:58:28 +0400

    Links from index and generating invites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :invites
   has_secure_password
   validates_format_of :login, with: /\A[a-z0-9_]{1,30}\z/
   validates_uniqueness_of :login
 
   before_validation :normalize_login
+
+  def generate_invite
+    Invite.create(user: self)
+  end
 
   protected
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,33 @@ require 'spec_helper'
 describe User do
   before(:each) { @user = FactoryGirl.build(:user, login: 'random_guy') }
 
-  it 'is invalid when login does not match pattern /\A[a-z0-9_]{1,30}\z/' do
-    @user.login = 'bad login'
-    expect(@user).not_to be_valid
+  context "model basics" do
+    it 'is invalid when login does not match pattern /\A[a-z0-9_]{1,30}\z/' do
+      @user.login = 'bad login'
+      expect(@user).not_to be_valid
+    end
+
+    it 'converts login to lowercase before validating' do
+      @user.valid?
+      expect(@user.login).to eq(@user.login.downcase)
+    end
+
+    it 'has unique login' do
+      @user.save!
+      another_user = FactoryGirl.build(:user, login: 'Random_guy')
+      expect(another_user).not_to be_valid
+    end
   end
 
-  it 'converts login to lowercase before validating' do
-    @user.valid?
-    expect(@user.login).to eq(@user.login.downcase)
-  end
+  context "generating invites" do
+    it "can generate invite" do
+      expect { @user.generate_invite }.to change(Invite, :count).by(1)
+    end
 
-  it 'has unique login' do
-    @user.save!
-    another_user = FactoryGirl.build(:user, login: 'Random_guy')
-    expect(another_user).not_to be_valid
+    it "is owner of generated invites" do
+      @user.generate_invite
+
+      expect(Invite.last.user).to eq(@user)
+    end
   end
 end
